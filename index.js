75d05685c7f7a189a8d6b8bd2f9f7974745f2d1a
WuShun <1109191769@qq.com>
2019-01-25 18:10:32 +0800

    创建数据库

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * 小肥牛扫码点餐系统API系统
+ */
+
+
+ const port = 8090;
+ const express = require('express');
+
+ var app = express();
+ app.listen(PORT,()=>{
+    console.log("Server listening"+PORT+'...')
+
+ })

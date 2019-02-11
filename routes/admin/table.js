fa04effa6f2c9010ee96c92d67c69bbcdb0e93c5
WuShun <wushun924@163.com>
2019-02-11 15:37:58 +0800

    稍

--- routes/admin/table.js
+++ routes/admin/table.js
@@ -1,40 +1,23 @@
 /*
-*桌台相关路由
+*桌台相关的路由器
 */
 const express = require('express');
 const pool = require('../../pool');
 var router = express.Router();
 module.exports = router;
 
-/**
- * GET /admin/table
- * 获取所有桌台信息
- * 返回数据
- * [
- * {tid:xxx,tname:'xxx',status:''}
- * ]
- */
-router.get('/',(req,res)=>{
-    pool.query('select * from xfn_table order by tid',(err,result)=>{
-        if(err)throw err;
-        res.send(result);
-    })
-
+/*
+*GET  /admin/table
+*获取所有的桌台信息
+*返回数据：
+*   [
+*     {tid:xxx, tname:'xxx', status:''},
+*     ...
+*   ]
+*/
+router.get('/', (req, res)=>{
+  pool.query('SELECT * FROM xfn_table ORDER BY tid', (err, result)=>{
+    if(err)throw err;
+    res.send(result);
+  })
 })
-
-
-
-router.get('/',(req,res)=>{
-    pool.query('select *from xfn_setting limit 1',(err,result)=>{
-        if(err) throw err;
-        res.send(result[0]);
-    })
-})
-
-router.put('/',(req,res)=>{
-    pool.query('update xfn_setting set ?',req.body,(err,result)=>{
-        if(err) throw err;
-        res.send({code:200,msg:'setting update success'});
-    })
-})
-
